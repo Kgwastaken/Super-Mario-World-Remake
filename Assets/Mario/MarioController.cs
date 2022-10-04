77315027020af32814d7ea6fddc0cca1cc7f270f
Daniel Peterson <1depeterson@seattleschools.org>
2022-10-04 09:44:51 -0700

    mario

--- Assets/Mario/MarioController.cs
+++ Assets/Mario/MarioController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MarioController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+{   
+    public float speed = 20f;
+    private float moveDirection;
+    private Rigidbody rb;
+    private bool facingRight = true;
+
+
+    private void Awake () {
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        moveDirection = Input.GetAxis("Horizontal");
+        if (moveDirection > 0 && !facingRight) {
+            flipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight) {
+            flipCharacter();
+        }
+        rb.velocity = new Vector2(moveDirection * speed, rb.velocity.y);
+    }
+
+    private void flipCharacter() {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 100f, 0f);
     }
 }
